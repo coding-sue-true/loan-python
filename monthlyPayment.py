ea8ca192a380971a840547a93e36a0614aa60f6b
soraia.carmo <soraia.carmo@10xbanking.com>
2019-06-12 15:11:48 +0100

    added condition to avoid ZeroDivisionError

--- monthlyPayment.py
+++ monthlyPayment.py
@@ -1,12 +1,12 @@
 def calculateMonthlyPayments(principal, annualInterestRate, duration):
     r = annualInterestRate/100/12
     n = duration * 12
-    if annualInterestRate == 0:
+    if annualInterestRate == 0 or r == 0:
         return principal/n
     secondPart = (r * ((1+r)**n)) / ((1+r)**n - 1)
     monthlyPayment = principal * secondPart
-    print(monthlyPayment)
     return monthlyPayment
 
 
-calculateMonthlyPayments(1000.0, 4.5, 5)
+# calculateMonthlyPayments(1000.0, 4.5, 5)
+# 18.6430192415
--- remainingBalance.py
+++ remainingBalance.py
@@ -2,14 +2,14 @@ def calculateRemainingBalance(principal, annualInterestRate, duration, numberOfP
     r = annualInterestRate/100/12
     n = duration * 12
     p = numberOfPayments
-    if annualInterestRate == 0:
+    if annualInterestRate == 0 or r == 0:
         return principal-numberOfPayments*(principal/n)
     top = (((1+r)**n) - ((1+r)**p))
     bottom = (((1+r)**n) - 1)
     remainingBalance = principal * (top/bottom)
-
-    print(remainingBalance)
+    # print(remainingBalance)
     return remainingBalance
 
 
-calculateRemainingBalance(1000.0, 4.5, 5, 12)
+# calculateRemainingBalance(1000.0, 4.5, 5, 12)
+# 817.551280458
