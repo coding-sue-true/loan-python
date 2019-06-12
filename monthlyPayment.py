95524541cc8839f9d1129a8db2cfe6a3a70eced4
soraia.carmo <soraia.carmo@10xbanking.com>
2019-06-12 11:15:44 +0100

    moved directory

--- monthlyPayment.py
+++ monthlyPayment.py
@@ -1,6 +1,8 @@
 def calculateMonthlyPayments(principal, annual_interest_rate, duration):
     r = ((annual_interest_rate/100)/12)
     n = duration * 12
+    if annual_interest_rate == 0:
+        return principal/n
     secondPart = (r * ((1+r)**n)) / ((1+r)**n - 1)
     monthlyPayment = principal * secondPart
     print(monthlyPayment)
